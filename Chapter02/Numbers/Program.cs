53a4aac40b36a2b2344c1cec8d69a0c4bd55e147
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-17 15:06:15 +0000

    Explore number sizes

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// An unsigned integer is a positive whole number or 0.
+uint naturalNumber = 23;
+
+// An integer is a negative or positive whole number or 0.
+int integerNumber = -23;
+
+// A float is a single-precision floating-point number.
+// The F or f suffix makes the value a float literal.
+// The suffix is required to compile.
+float realNumber = 2.3f;
+
+// A double is a double-precision floating-point number.
+// double is the default for a number value with a decimal point.
+double anotherRealNumber = 2.3; // A double literal value.
+
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+// Check the three variables have the same value.
+Console.WriteLine($"{decimalNotation == binaryNotation}");
+Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+
+// Output the variable values in binary.
+Console.WriteLine($"{Convert.ToString(decimalNotation, 2)}");
+Console.WriteLine($"{Convert.ToString(binaryNotation, 2)}");
+Console.WriteLine($"{Convert.ToString(hexadecimalNotation, 2)}");
+
+// Output the variable values in decimal.
+Console.WriteLine($"{decimalNotation:N0}");
+Console.WriteLine($"{binaryNotation:N0}");
+Console.WriteLine($"{hexadecimalNotation:N0}");
+
+// Output the variable values in hexadecimal.
+Console.WriteLine($"{decimalNotation:X}");
+Console.WriteLine($"{binaryNotation:X}");
+Console.WriteLine($"{hexadecimalNotation:X}");
+
+Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}.");
+Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
