a964fa588a2e0fc79f8d2fa5bcd9d29336f7ea95
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-19 02:00:52 +0000

    Get key input from the user

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -43,3 +43,9 @@ Console.Write("Type your age and press ENTER: ");
 string age = Console.ReadLine()!;
 
 Console.WriteLine($"Hello {firstName}, you look good for {age}.");
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key, arg1: key.KeyChar, arg2: key.Modifiers);
