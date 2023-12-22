aecff94c8e05444cefa65572e2a6b7924a849d90
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-22 02:43:22 +0000

    Looping with the for statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -14,3 +14,13 @@ while (x < 10)
 // }
 // while (password != actualPassword);
 // WriteLine("Correct!");
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
