8cdf35203a84b32fc1f404de0983bb373778f0d6
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-22 02:45:13 +0000

    Looping with the foreach statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -24,3 +24,9 @@ for (int y = 0; y <= 10; y += 3)
 {
     WriteLine(y);
 }
+
+string[] names = { "Adam", "Barry", "Charlie" };
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
