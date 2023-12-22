0f0d76c12cce9cb01b96f42e225595b49d03b907
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-22 16:04:25 +0000

    Cast numbers implicitly and explicitly

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 10;
+double b = a; // An int can be safely cast into a double.
+WriteLine($"a is {a}, b is {b}");
+
+double c = 9.8;
+int d = (int)c; // Compiler gives an error if you do not explicitly cast.
+WriteLine($"c is {c}, d is {d}"); // d loses the .8 part.
+
+long e = 10;
+int f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
+
+e = 5_000_000_000;
+f = (int)e;
+WriteLine($"e is {e:N0}, f is {f:N0}");
