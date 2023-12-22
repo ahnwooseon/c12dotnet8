3a510f2d42745462d1df5ba4151699b6fb5bcf62
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-22 18:33:32 +0000

    Round numbers and the default rounding rules

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -1,4 +1,6 @@
-﻿int a = 10;
+﻿using static System.Convert;
+
+int a = 10;
 double b = a; // An int can be safely cast into a double.
 WriteLine($"a is {a}, b is {b}");
 
@@ -21,3 +23,29 @@ for (int i = 8; i >= -8; i--)
     WriteLine("{0,12} {0,34:B32}", i);
 }
 WriteLine("{0,12} {0,34:B32}", int.MinValue);
+
+double g = 9.8;
+int h = ToInt32(g);
+WriteLine($"g is {g}, h is {h}");
+
+double[,] doubles = {
+    { 9.49, 9.5, 9.51 },
+    { 10.49, 10.5, 10.51 },
+    { 11.49, 11.5, 11.51 },
+    { 12.49, 12.5, 12.51 } ,
+    { -12.49, -12.5, -12.51 },
+    { -11.49, -11.5, -11.51 },
+    { -10.49, -10.5, -10.51 },
+    { -9.49, -9.5, -9.51 }
+};
+
+WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7} ");
+    }
+    WriteLine("|");
+}
+WriteLine();
