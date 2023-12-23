337bf6b3dd92171ea4e25cc902c9b8aaa7d4e416
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-23 06:57:44 +0000

    Disable compiler overflow checks with the unchecked statement

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -56,3 +56,13 @@ checked
         WriteLine("The code overflowed but I caught the exception.");
     }
 }
+
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
