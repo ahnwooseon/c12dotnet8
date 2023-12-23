38078d637c5f81b31034bbebaf5f35b16b0b1f64
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-23 06:44:44 +0000

    Throw overflow exceptions with the checked statement

--- Chapter03/HandlingExceptions/Program.cs
+++ Chapter03/HandlingExceptions/Program.cs
@@ -37,3 +37,22 @@ catch (FormatException)
 {
     WriteLine("Amounts must only contain digits!");
 }
+
+checked
+{
+    try
+    {
+        int x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+    catch (OverflowException)
+    {
+        WriteLine("The code overflowed but I caught the exception.");
+    }
+}
