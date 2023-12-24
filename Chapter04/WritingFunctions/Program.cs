22934d6303cff17ce1d3e86f11a2b5db4db5a503
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-24 09:09:07 +0000

    Converting numbers from cardinal to ordinal

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,10 +1,12 @@
 ﻿// TimesTable(7, 20);
 
-ConfigureConsole("fr-BE");
+// ConfigureConsole("fr-BE");
 
-decimal taxToPay = CalculateTax(149, "FR");
-WriteLine($"You must pay {taxToPay:C} in tax.");
+// decimal taxToPay = CalculateTax(149, "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
 
 // Alternatively, call the function in the interpolated string.
 // WriteLine($"You must pay {CalculateTax(amount: 149,
 // twoLetterRegionCode: "FR"):C} in tax.");
+
+RunCardinalToOrdinal();
