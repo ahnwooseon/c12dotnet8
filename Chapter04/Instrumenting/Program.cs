0c4755dfdc090d784e8feba398fae14bdad544b2
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-25 00:22:48 +0000

    Write to the default trace listener

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics; // To use Debug and Trace.
+
+Debug.WriteLine("Debug says, I am watching!");
+Trace.WriteLine("Trace says, I am watching!");
