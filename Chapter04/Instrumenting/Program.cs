7dc2cbfd8648ef9205c9d786b33a1a4fc869f970
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-25 00:32:59 +0000

    Configure trace listeners

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,23 @@
 ﻿using System.Diagnostics; // To use Debug and Trace.
 
+string logPath = Path.Combine(Environment.GetFolderPath(
+    Environment.SpecialFolder.DesktopDirectory), "log.txt");
+
+Console.WriteLine($"Writing to: {logPath}");
+
+TextWriterTraceListener logFile = new(File.CreateText(logPath));
+
+Trace.Listeners.Add(logFile);
+
+#if DEBUG
+// Text writer is buffered, so this option calls
+// Flush() on all listeners after writing.
+Trace.AutoFlush = true;
+#endif
+
 Debug.WriteLine("Debug says, I am watching!");
 Trace.WriteLine("Trace says, I am watching!");
+
+// Close the text file (also flushes) and release resources.
+Debug.Close();
+Trace.Close();
