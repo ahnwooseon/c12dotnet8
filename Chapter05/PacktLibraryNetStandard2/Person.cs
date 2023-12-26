67a13a877b1b54a631a891df1e2894aae16d923f
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-26 21:55:50 +0000

    Control how parameters are passed

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -71,5 +71,20 @@ public class Person : object
             active);
     }
 
+    public void PassingParameters(int w, in int x, ref int y, out int z)
+    {
+        // out parameters cannot have a default and they
+        // must be initialized inside the method.
+        z = 100;
+
+        // Increment each parameter except the read-only x.
+        w++;
+        // x++; // Gives a compiler error!
+        y++;
+        z++;
+
+        WriteLine($"In the method: w={w}, x={x}, y={y}, z={z}");
+    }
+
     #endregion
 }
