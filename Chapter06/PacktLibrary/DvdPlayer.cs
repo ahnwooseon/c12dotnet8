7c418d095f29539a1ee6207a211a3ebecb6d1211
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-28 04:57:29 +0000

    Define interfaces with default implementations

---
+++ Chapter06/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,13 @@
+namespace Packt.Shared;
+public class DvdPlayer : IPlayable
+{
+    public void Pause()
+    {
+        WriteLine("DVD player is pausing.");
+    }
+
+    public void Play()
+    {
+        WriteLine("DVD player is playing.");
+    }
+}
