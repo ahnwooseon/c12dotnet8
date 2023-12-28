eef734a4c7f0f8be67b9f62d7245c3330d23bea6
Ahn Wooseon <ahnwooseon@gmail.com>
2023-12-28 01:28:38 +0000

    Set up class library and console application

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25,
+        hour: 0, minute: 0, second: 0,
+        offset: TimeSpan.Zero)
+};
+
+harry.WriteToConsole();
