89b0e84a9482c6001960f2783cd08a55fd5adc15
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-25 20:56:54 +0000

    Exploring unary operators

--- Chapter03/Operators/Program.cs
+++ Chapter03/Operators/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Exploring unary operators
+
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // Prefix means increment c before assigning it.
+WriteLine($"c is {c}, d is {d}");
+
+#endregion
