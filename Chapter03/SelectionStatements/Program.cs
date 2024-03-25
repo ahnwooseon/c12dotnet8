3ae194f1d69b6c74606e9854f4bca03275e80afc
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-25 22:13:05 +0000

    Branching with the if statement

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string password = "ninja";
+if (password.Length < 8)
+{
+    WriteLine("Your password is too short. Use at least 8 chars.");
+}
+else
+{
+    WriteLine("Your password is strong.");
+}
