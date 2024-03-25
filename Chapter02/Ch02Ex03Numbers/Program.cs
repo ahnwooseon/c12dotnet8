5936dfd828b1d0e00e25da835219f05e002b6339
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-25 09:25:32 +0000

    Practice number sizes and ranges

--- Chapter02/Ch02Ex03Numbers/Program.cs
+++ Chapter02/Ch02Ex03Numbers/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using System.Threading.Tasks.Dataflow;
+
+List<Type> types = new()
+{
+    typeof(sbyte),
+    typeof(byte),
+    typeof(short),
+    typeof(ushort),
+    typeof(int),
+    typeof(uint),
+    typeof(long),
+    typeof(ulong),
+    /*typeof(Int128),
+    typeof(UInt128),
+    typeof(Half),*/
+    typeof(float),
+    typeof(double),
+    typeof(decimal)
+};
+
+WriteLine(new string('-', 86));
+WriteLine($"{"Type",-8}{"Byte(s) of memory",-10}{"Min",23}{"Max",38}");
+WriteLine(new string('-', 86));
+foreach (Type type in types)
+{
+    Write($"{type.Name.ToLower(),-8}");
+    Write($"{Marshal.SizeOf(type).ToString(), -10}");
+    Write($"{type.GetField("MinValue").GetValue(null).ToString(),30}");
+    WriteLine($"{type.GetField("MaxValue").GetValue(null).ToString(),38}");
+}
+WriteLine(new string('-', 86));
