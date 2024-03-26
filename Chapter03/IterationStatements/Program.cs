6d87c66bb3eca317aed474acf8b2dd186fc228e1
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-26 08:57:39 +0000

    Looping with the do statement

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+// string? actualPassword = "Pa$$w0rd";
+// string? password;
+// int numberOfAttempts = 0;
+
+// do
+// {
+//     Write("Enter your password: ");
+//     password = ReadLine();
+//     numberOfAttempts++;
+// }
+// while (password != actualPassword && numberOfAttempts < 3);
+
+// if (numberOfAttempts < 3)
+// {
+//     WriteLine("Correct!");
+// }
+// else
+// {
+//     WriteLine("Maximum number of attempts exceeded.");
+// }
