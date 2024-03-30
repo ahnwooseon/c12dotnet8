a09408f1c3676e710c03d680637c6fb5129f0c36
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-30 12:35:42 +0000

    Test your knowledge of operators

--- Chapter03/Ch03Ex03Operators/Program.cs
+++ Chapter03/Ch03Ex03Operators/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int x = 3;
+int y = 2 + ++x;
+WriteLine($"{x} {y}");
+
+x = 3 << 2;
+y = 10 >> 1;
+WriteLine($"{x} {y}");
+
+x = 10 & 8;
+y = 10 | 7;
+WriteLine($"{x} {y}");
+WriteLine($"{x:b} {y:b}");
