a50894386f1851c20e891b67c287e6dee0730baa
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-30 11:01:44 +0000

    Explore loops and overflow

--- Chapter03/Ch03Ex02LoopsAndOverflow/Program.cs
+++ Chapter03/Ch03Ex02LoopsAndOverflow/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿checked
+{
+    int max = 500;
+    for (byte i = 0; i < max; i++)
+    {
+        WriteLine(i);
+    }
+}
