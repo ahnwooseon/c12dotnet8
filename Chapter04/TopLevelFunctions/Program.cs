31a0d076f7a6795a60c01082b7321c9dc3a6d098
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-31 22:49:00 +0000

    Defining a partial Program class with a static function

--- Chapter04/TopLevelFunctions/Program.cs
+++ Chapter04/TopLevelFunctions/Program.cs
@@ -3,9 +3,3 @@
 WriteLine("* Top-level functions example");
 
 WhatsMyNamespace(); // Call the function.
-
-void WhatsMyNamespace() // Define a local function.
-{
-    WriteLine(format: "Namespace of Program class: {0}",
-        arg0: typeof(Program).Namespace ?? "null");
-}
