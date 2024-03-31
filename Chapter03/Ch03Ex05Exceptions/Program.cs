e1a6ca6743f9d889e1c8cb5d68d3ecb91ce7af90
Ahn Wooseon <ahnwooseon@gmail.com>
2024-03-31 19:13:31 +0000

    Practice exception handling

--- Chapter03/Ch03Ex05Exceptions/Program.cs
+++ Chapter03/Ch03Ex05Exceptions/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Write("Enter a number between 0 and 255: ");
+string number = ReadLine()!;
+Write("Enter another number between 0 and 255: ");
+string anotherNumber = ReadLine()!;
+
+try
+{
+    byte parsedNumber = byte.Parse(number);
+    int parsedAnotherNumber = int.Parse(anotherNumber);
+    WriteLine($"{number} divided by {anotherNumber} is {parsedNumber / parsedAnotherNumber}");
+}
+catch (Exception e)
+{
+    WriteLine($"{e.GetType().Name}: {e.Message}");
+}
