9520f6660291d147ee61b0e8e2abcf940e9c5933
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-01 05:10:47 +0000

    Writing a function that returns a value

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,9 @@
-﻿TimesTable(10, 10);
+﻿// TimesTable(10, 10);
+
+ConfigureConsole(culture: "fr-FR");
+
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay:C} in tax.");
+
+// Alternatively, call the function in the interpolated string.
+// WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
