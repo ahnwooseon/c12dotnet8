4518c940796e5dde53e02a6e02a2a0e1dd7eb402
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-01 16:16:22 +0000

    Creating code with a deliberate bug

--- Chapter04/Debugging/Program.cs
+++ Chapter04/Debugging/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine(); // Wait for user to press Enter.
+
+double Add(double a, double b)
+{
+    return a * b; // Deliberate bug!
+}
