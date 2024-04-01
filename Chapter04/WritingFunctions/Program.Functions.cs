de0550fce8854f69e8032f910dfc6a027787465b
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-01 05:21:22 +0000

    Converting numbers from cardinal to ordinal

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -40,4 +40,36 @@ partial class Program
         }
         WriteLine($"CurrentCulture: {CultureInfo.CurrentCulture.DisplayName}");
     }
+
+    static string CardinalToOrdinal(uint number)
+    {
+        uint lastTwoDigits = number % 100;
+
+        switch (lastTwoDigits)
+        {
+            case 11: // Special cases for 11th to 13th.
+            case 12:
+            case 13:
+                return $"{number:N0}th";
+            default:
+                uint lastDigit = number % 10;
+                string suffix = lastDigit switch
+                {
+                    1 => "st",
+                    2 => "nd",
+                    3 => "rd",
+                    _ => "th"
+                };
+                return $"{number:N0}{suffix}";
+        }
+    }
+
+    static void RunCardinalToOrdinal()
+    {
+        for (uint number = 1; number <= 1500; number++)
+        {
+            Write($"{CardinalToOrdinal(number)} ");
+        }
+        WriteLine();
+    }
 }
--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,9 +1,11 @@
 ﻿// TimesTable(10, 10);
 
-ConfigureConsole(culture: "fr-FR");
+// ConfigureConsole(culture: "fr-FR");
 
-decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
-WriteLine($"You must pay {taxToPay:C} in tax.");
+// decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+// WriteLine($"You must pay {taxToPay:C} in tax.");
 
 // Alternatively, call the function in the interpolated string.
 // WriteLine($"You must pay {CalculateTax(amount: 149, twoLetterRegionCode: "FR"):C} in tax.");
+
+RunCardinalToOrdinal();
