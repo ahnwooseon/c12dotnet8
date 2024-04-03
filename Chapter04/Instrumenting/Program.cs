9a87d587714c24d94cb5fd1503b22b9fa310fe2e
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-03 09:17:28 +0000

    Logging information about your source code

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -49,6 +49,9 @@ Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
 Trace.WriteLineIf(ts.TraceInfo, "Trace information");
 Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
 
+int unitsInStock = 12;
+LogSourceDetails(unitsInStock > 10);
+
 // Close the text file (also flushes) and release resources.
 Debug.Close();
 Trace.Close();
