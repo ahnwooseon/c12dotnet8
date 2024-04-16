44aa9951cb3f10fc6958452d2ccd74b541e88aa8
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-16 08:28:47 +0000

    Implementing PrimeFactors class

---
+++ Chapter04/Ch04Ex02PrimeFactorsLib/PrimeFactors.cs
@@ -0,0 +1,28 @@
+﻿namespace Ch04Ex02PrimeFactorsLib;
+
+public class PrimeFactors
+{
+    public List<int> Factorize(int n)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegative(n, nameof(n));
+        
+        List<int> result = new();
+
+        for (int i = 2; i <= Math.Sqrt(n); i++)
+        {
+            while (n % i == 0)
+            {
+                result.Add(i);
+                n /= i;
+            }
+        }
+        if (n > 2)
+        {
+            result.Add(n);
+        }
+
+        result.Reverse();
+
+        return result;
+    }
+}
