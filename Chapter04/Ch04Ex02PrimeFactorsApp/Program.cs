14beaaa60e1685dfbc1823ed1a90f937471ed1cc
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-16 14:22:25 +0000

    Implement console app

--- Chapter04/Ch04Ex02PrimeFactorsApp/Program.cs
+++ Chapter04/Ch04Ex02PrimeFactorsApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ch04Ex02PrimeFactorsLib;
+using static System.Console;
+
+Write("""
+* Prime Factors App *
+Enter a number: 
+""");
+string number = ReadLine()!;
+
+ArgumentException.ThrowIfNullOrWhiteSpace(number, nameof(number));
+
+List<int> primeFactors = new PrimeFactors().Factorize(int.Parse(number));
+
+WriteLine($"The prime factors of {number} are: {string.Join(", ", primeFactors)}");
