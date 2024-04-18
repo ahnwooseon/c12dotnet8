803b21c00d4808b7a0805d03b7f7ba7769a9a57c
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-18 07:28:08 +0000

    Instantiating a class

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared; // To use Person.
+
+ConfigureConsole(); // Sets current culture to US English.
+
+// Alternatives:
+// ConfigureConsole(useComputerCulture: true); // Use your culture.
+// ConfigureConsole(culture: "fr-FR"); // Use French culture.
+
+// Person bob = new Person(); // C# 1 or later.
+// var bob = new Person(); // C# 3 or later.
+
+Person bob = new(); // C# 9 or later.
+WriteLine(bob); // Implicit call to ToString().
+// WriteLine(bob.ToString()); // Does the same thing.
