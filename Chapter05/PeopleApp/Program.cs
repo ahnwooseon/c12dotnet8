af928f54b241f06990ad324251aa079c9004ac05
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-18 21:34:31 +0000

    Making a field static

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -65,3 +65,19 @@ foreach (Person child in bob.Children)
 {
     WriteLine($"> {child.Name}");
 }
+
+BankAccount.InterestRate = 0.012M; // Store a shared value in static field.
+
+BankAccount jonesAccount = new();
+jonesAccount.AccountName = "Mrs. Jones";
+jonesAccount.Balance = 2400;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: jonesAccount.AccountName,
+    arg1: jonesAccount.Balance * BankAccount.InterestRate);
+
+BankAccount gerrierAccount = new();
+gerrierAccount.AccountName = "Ms. Gerrier";
+gerrierAccount.Balance = 98;
+WriteLine(format: "{0} earned {1:C} interest.",
+    arg0: gerrierAccount.AccountName,
+    arg1: gerrierAccount.Balance * BankAccount.InterestRate);
