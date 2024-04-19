d5fd9a5506a6703529b6cb7fac5d17e1b1829fa2
Ahn Wooseon <ahnwooseon@gmail.com>
2024-04-19 08:43:37 +0000

    Setting required fields with a constructor

--- Chapter05/PacktLibraryModern/Book.cs
+++ Chapter05/PacktLibraryModern/Book.cs
@@ -1,4 +1,6 @@
-﻿namespace Packt.Shared;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Packt.Shared;
 
 public class Book
 {
@@ -9,4 +11,15 @@ public class Book
     // Works with any version of .NET.
     public string? Author;
     public int PageCount;
+
+    // Constructor for use with object initializer syntax.
+    public Book() { }
+
+    // Constructor with parameters to set required fields.
+    [SetsRequiredMembers]
+    public Book(string? isbn, string? title)
+    {
+        Isbn = isbn;
+        Title = title;
+    }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -88,10 +88,19 @@ WriteLine($"{bob.Name} is a {Person.Species}.");
 // Read-only fields are accessible via the variable.
 WriteLine($"{bob.Name} was born on {bob.HomePlanet}.");
 
+/*
+// Instantiate a book using object initializer syntax.
 Book book = new()
 {
     Isbn = "978-1803237800",
     Title = "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals"
+};
+*/
+Book book = new(isbn: "978-1803237800",
+    title: "C# 12 and .NET 8 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J. Price",
+    PageCount = 821
 };
 
 WriteLine("{0}: {1} written by {2} has {3:N0} pages.",
