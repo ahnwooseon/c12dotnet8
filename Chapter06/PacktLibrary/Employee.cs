296696576ea2b7b146080707dd01648a4a2dca4b
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-07 09:40:24 +0000

    Hiding members

--- Chapter06/PacktLibrary/Employee.cs
+++ Chapter06/PacktLibrary/Employee.cs
@@ -4,4 +4,11 @@ public class Employee : Person
 {
     public string? EmployeeCode { get; set; }
     public DateOnly HireDate { get; set; }
+
+    public new void WriteToConsole()
+    {
+        WriteLine(
+            format: "{0} was born on {1:dd/MM/yy} and hired on {2:dd/MM/yy}.",
+            arg0: Name, arg1: Born, arg2: HireDate);
+    }
 }
