00fa4e91aafc2f623bdd397ebf725c307c210135
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-09 14:10:03 +0000

    Building a native AOT project

--- Chapter07/AotConsole/Program.cs
+++ Chapter07/AotConsole/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Globalization; // To use CultureInfo.
+
+WriteLine("This is an ahead-of-time (AOT) compiled console app.");
+WriteLine("Current culture: {0}", CultureInfo.CurrentCulture.  DisplayName);
+WriteLine("OS version: {0}", Environment.OSVersion);
+
+Write("Press any key to exit.");
+ReadKey(intercept: true); // Do not output the key that was pressed.
