a241ea489136eea34f60d7905ab8d77f58459ab7
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-12 23:04:53 +0000

    Splitting a string

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -2,3 +2,14 @@
 WriteLine($"{city} is {city.Length} characters long.");
 
 WriteLine($"First char is {city[0]} and fourth is {city[3]}.");
+
+string cities = "Paris,Tehran,Chennai,Sydney,New York,Medellín";
+
+string[] citiesArray = cities.Split(',');
+
+WriteLine($"There are {citiesArray.Length} items in the array:");
+
+foreach (string item in citiesArray)
+{
+    WriteLine($"  {item}");
+}
