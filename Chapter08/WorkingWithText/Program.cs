2a6d73e106a8ae4f368e4a0216027f16c8636ac4
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-12 23:03:15 +0000

    Getting the length of a string

--- Chapter08/WorkingWithText/Program.cs
+++ Chapter08/WorkingWithText/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string city = "London";
+WriteLine($"{city} is {city.Length} characters long.");
