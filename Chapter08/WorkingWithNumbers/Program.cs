de599b796247867b59b984687ce231c3d8eb974b
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-12 22:58:40 +0000

    Generating GUIDs

--- Chapter08/WorkingWithNumbers/Program.cs
+++ Chapter08/WorkingWithNumbers/Program.cs
@@ -61,3 +61,15 @@ foreach (string beatle in beatles)
     Write($" {beatle}");
 }
 WriteLine();
+
+WriteLine($"Empty GUID: {Guid.Empty}.");
+Guid g = Guid.NewGuid();
+WriteLine($"Random GUID: {g}.");
+
+byte[] guidAsBytes = g.ToByteArray();
+Write("GUID as byte array: ");
+for (int i = 0; i < guidAsBytes.Length; i++)
+{
+    Write($"{guidAsBytes[i]:X2} ");
+}
+WriteLine();
