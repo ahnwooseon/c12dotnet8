0418037b3a2c347b8d5f1f745263b3f8854004c3
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-23 09:31:57 +0000

    Working with lists

--- Chapter08/WorkingWithCollections/Program.cs
+++ Chapter08/WorkingWithCollections/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Simple syntax for creating a list and adding three items.
+List<string> cities = new();
+cities.Add("London");
+cities.Add("Paris");
+cities.Add("Milan");
+
+/* Alternative syntax that is converted by the compiler into
+   the three Add method calls above.
+List<string> cities = new() { "London", "Paris", "Milan" }; */
+
+/* Alternative syntax that passes an array
+   of string values to AddRange method.
+List<string> cities = new();
+cities.AddRange(new[] { "London", "Paris", "Milan" }); */
+
+OutputCollection("Initial list", cities);
+WriteLine($"The first city is {cities[0]}.");
+WriteLine($"The last city is {cities[cities.Count - 1]}.");
+
+cities.Insert(0, "Sydney");
+OutputCollection("After inserting Sydney at index 0", cities);
+
+cities.RemoveAt(1);
+cities.Remove("Milan");
+OutputCollection("After removing two cities", cities);
