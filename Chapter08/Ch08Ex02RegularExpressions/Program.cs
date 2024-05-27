78b853b512dcca6b596fc20442f1e755fd2caa60
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-27 19:35:53 +0000

    Practice regular expressions

--- Chapter08/Ch08Ex02RegularExpressions/Program.cs
+++ Chapter08/Ch08Ex02RegularExpressions/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+WriteLine("The default regular expression checks for at least one digit.");
+
+do
+{
+    Check();
+}
+while (ReadKey().Key != ConsoleKey.Escape);
+WriteLine();
+
+void Check()
+{
+    Write("Enter a regular expression (or press ENTER to use the default): ");
+    string regex = @$"{ReadLine()}";
+    if (string.IsNullOrWhiteSpace(regex)) regex = @"^[a-z]+$";
+    Write("Enter some input: ");
+    string input = ReadLine();
+    WriteLine($"{input} matches {regex}? {new Regex(regex).IsMatch(input)}");
+    Write("Press ESC to end or any key to try again.");
+}
