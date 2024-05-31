0eaeaa49726427ff10db70ddcd31aa81db5afb34
Ahn Wooseon <ahnwooseon@gmail.com>
2024-05-31 20:54:28 +0000

    Writing to XML streams

--- Chapter09/WorkingWithStreams/Program.cs
+++ Chapter09/WorkingWithStreams/Program.cs
@@ -1,4 +1,5 @@
-﻿using Packt.Shared; // To use Viper.
+﻿using System.Xml; // To use XmlWriter and so on.
+using Packt.Shared; // To use Viper.
 
 SectionTitle("Writing to text streams");
 
@@ -17,3 +18,56 @@ foreach (string item in Viper.Callsigns)
 text.Close(); // Release unmanaged file resources.
 
 OutputFileInfo(textFile);
+
+SectionTitle("Writing to XML streams");
+
+// Define a file path to write to.
+string xmlFile = Combine(CurrentDirectory, "streams.xml");
+
+// Declare variables for the filestream and XML writer.
+FileStream? xmlFileStream = null;
+XmlWriter? xml = null;
+
+try
+{
+    xmlFileStream = File.Create(xmlFile);
+
+    // Wrap the file stream in an XML writer helper and tell it
+    // to automatically indent nested elements.
+    xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+
+    // Write the XML declaration.
+    xml.WriteStartDocument();
+
+    // Write a root element.
+    xml.WriteStartElement("callsigns");
+
+    // Enumerate the strings, writing each one to the stream.
+    foreach (string item in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", item);
+    }
+
+    // Write the close root element.
+    xml.WriteEndElement();
+}
+catch (Exception ex)
+{
+    // If the path doesn't exist the exception will be caught.
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+finally
+{
+    if (xml is not null)
+    {
+        xml.Close();
+        WriteLine("The XML writer's unmanaged resources have been disposed.");
+    }
+    if (xmlFileStream is not null)
+    {
+        xmlFileStream.Close();
+        WriteLine("The file stream's unmanaged resources have been disposed.");
+    }
+}
+
+OutputFileInfo(xmlFile);
